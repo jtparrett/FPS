aa7ef6b697a60b3969c9c74ce9ebd2e8a6a09654
Josh Parrett <jtparrett@me.com>
2016-01-27 16:26:36 +0000

    colour theme

--- fps.js
+++ fps.js
@@ -15,10 +15,8 @@ FPS.prototype = {
     this.context = this.el.getContext('2d');
     this.el.width = this.width;
     this.el.height = this.height;
-    
     this.context.font = "16px Arial";
-
-    this.el.setAttribute('style', 'position: fixed;top: 0;right: 0;background: red;z-index: 200');
+    this.el.setAttribute('style', 'position:fixed;top:0;right:0;background:#973252;z-index:200');
     document.body.appendChild(this.el);
   },
 
@@ -34,7 +32,7 @@ FPS.prototype = {
     var curFps = this.getFPS();
     var prevFps = curFps;
 
-    this.context.fillStyle = '#58fd09';
+    this.context.fillStyle = '#E297AF';
     this.context.clearRect(0, 0, this.width, this.height);
 
     for(var i = 0; i < this.width; i++){
@@ -44,7 +42,7 @@ FPS.prototype = {
       this.context.fillRect(this.width - i - 1, this.height - tmpPrevFps, 1, tmpPrevFps);
     }
 
-    this.context.fillStyle = '#000';
+    this.context.fillStyle = '#711331';
     this.context.fillText(curFps + ' FPS', 10, this.height - 10);
   },
 
@@ -53,7 +51,7 @@ FPS.prototype = {
 
     var d = new Date().getTime();
     var currentTime = (d - this.startTime) / 1000;
-    var result = Math.floor((this.frame / currentTime));
+    var result = Math.floor(this.frame / currentTime);
 
     if(currentTime > 1){
       this.startTime = new Date().getTime();
